99c1a4885b76e4e8e1cd6e74c67c829cb40ca8d3
TexTrue <65154269+TexBlock@users.noreply.github.com>
2024-04-20 19:41:10 +0800

    clean

---
+++ forge/src/main/java/org/thinkingstudio/ryoamiclights/forge/ForgeEventHandler.java
@@ -0,0 +1,32 @@
+package org.thinkingstudio.ryoamiclights.forge;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.resource.SynchronousResourceReloader;
+import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
+import net.minecraftforge.client.event.RenderLevelStageEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.thinkingstudio.ryoamiclights.RyoamicLights;
+import org.thinkingstudio.ryoamiclights.api.item.ItemLightSources;
+import org.thinkingstudio.ryoamiclights.forge.api.DynamicLightsInitializerEvent;
+
+public class ForgeEventHandler {
+    public static void registerEvents() {
+        IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        forgeEventBus.<RenderLevelStageEvent>addListener(EventPriority.HIGHEST, event -> {
+            if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_TRIPWIRE_BLOCKS) {
+                MinecraftClient.getInstance().getProfiler().swap("dynamic_lighting");
+                RyoamicLights.get().updateAll(event.getLevelRenderer());
+            }
+        });
+        modEventBus.<RegisterClientReloadListenersEvent>addListener(EventPriority.HIGHEST, event -> {
+            event.registerReloadListener((SynchronousResourceReloader) ItemLightSources::load);
+        });
+
+        forgeEventBus.post(new DynamicLightsInitializerEvent());
+    }
+}
--- forge/src/main/java/org/thinkingstudio/ryoamiclights/forge/RyoamicLightsForge.java
+++ forge/src/main/java/org/thinkingstudio/ryoamiclights/forge/RyoamicLightsForge.java
@@ -10,25 +10,14 @@
 
 package org.thinkingstudio.ryoamiclights.forge;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.resource.SynchronousResourceReloader;
 import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.client.ConfigGuiHandler;
-import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
-import net.minecraftforge.client.event.RenderLevelStageEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.network.NetworkConstants;
-import org.jetbrains.annotations.NotNull;
 import org.thinkingstudio.ryoamiclights.RyoamicLights;
-import org.thinkingstudio.ryoamiclights.api.item.ItemLightSources;
-import org.thinkingstudio.ryoamiclights.forge.api.DynamicLightsInitializerEvent;
 import org.thinkingstudio.ryoamiclights.gui.SettingsScreen;
 
 @Mod(RyoamicLights.NAMESPACE)
@@ -41,7 +30,6 @@ public class RyoamicLightsForge {
 
     public void onInitializeClient() {
         ModLoadingContext context = ModLoadingContext.get();
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         RyoamicLights.get().clientInit();
 
@@ -49,20 +37,7 @@ public class RyoamicLightsForge {
 
         context.registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         context.registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () -> new ConfigGuiHandler.ConfigGuiFactory((client, screen) -> new SettingsScreen(screen)));
-        modEventBus.addListener(EventPriority.HIGHEST, this::renderWorldLast);
-        modEventBus.addListener(EventPriority.HIGHEST, this::registerClientReloadListeners);
 
-        MinecraftForge.EVENT_BUS.post(new DynamicLightsInitializerEvent());
-    }
-
-    public void renderWorldLast(@NotNull RenderLevelStageEvent event) {
-        if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_TRIPWIRE_BLOCKS) {
-            MinecraftClient.getInstance().getProfiler().swap("dynamic_lighting");
-            RyoamicLights.get().updateAll(event.getLevelRenderer());
-        }
-    }
-
-    public void registerClientReloadListeners(RegisterClientReloadListenersEvent event) {
-        event.registerReloadListener((SynchronousResourceReloader) ItemLightSources::load);
+        ForgeEventHandler.registerEvents();
     }
 }
